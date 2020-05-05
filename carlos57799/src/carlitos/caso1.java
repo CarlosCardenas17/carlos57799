944a8c82f1aa967e765715584240a598826d194b
samuel <samuel@192.168.1.8>
2020-05-05 18:27:55 -0500

    carlos cardenas guia

--- carlos57799/src/carlitos/caso1.java
+++ carlos57799/src/carlitos/caso1.java
@@ -1,9 +1,10 @@
 package carlitos;
+
 public class caso1 {
 
-	public static void main(String[] args)
-	{
-         System.out.println("Bienvenido programador Senatino");
- }
-}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 
+	}
+
+}
