f35582f4fa6923b1b545ff82c87ece18a4d34752
samuel <samuel@192.168.1.8>
2020-05-05 18:43:19 -0500

    guia 1

--- carlos57799/src/carlitos/caso2.java
+++ carlos57799/src/carlitos/caso2.java
@@ -1,10 +1,21 @@
+
+
 package carlitos;
+import java.util.Scanner;
 
-public class caso2 {
+public class caso2
+{
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	public static void main(String[] args) 
+	{
+		Scanner sac = new Scanner(System.in);
+		
+		System.out.println("Ingrese nombre: ");
+		String nombre = sac.nextLine();
+		
+		System.out.println("Resultados");
+		System.out.println("------------------");
+		System.out.println("Número de caracteres: " + nombre);
+    }
 
 }
